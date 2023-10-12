707cb8fedc3c21d6017a848ec7e29efa59b5d74e
Dee.eff <itzdaniie@gmail.com>
2023-10-12 14:25:49 -0700

    Okay

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -6,5 +6,11 @@ c = 'A';
 printf("%c: %d\n", c, _isupper(c));
 c = 'a';
 printf("%c: %d\n", c, _isupper(c));
+
+char c;
+c = '0';
+printf("%c: %d\n", c, _isdigit(c));
+c = 'a';
+printf("%c: %d\n", c, _isdigit(c));
 return (0);
 }
