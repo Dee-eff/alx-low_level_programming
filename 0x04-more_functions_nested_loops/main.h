7e7bacab6528b5770c36f56100c1c5eae9194a92
Dee.eff <itzdaniie@gmail.com>
2023-10-12 14:04:30 -0700

    Okay

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * File: main.h
+ * Auth: Daniel Favour
+ * Desc: Header file containing declarations for all functions used in the 0x04-more_functions_nested_loops directory
+ *
+ */
+
+int _isupper(int c);
