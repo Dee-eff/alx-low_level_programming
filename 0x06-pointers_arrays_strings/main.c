b1ca81bdbbb26b3938e1b26c48c869e8c4c7e8e4
Dee.eff <itzdaniie@gmail.com>
2023-10-19 18:37:46 -0700

    Okay

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -89,5 +89,11 @@ ptr = string_toupper(str);
 printf("%s", ptr);
 printf("%s", str);
 
+char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+char *ptr;
+ptr = cap_string(str);
+printf("%s", ptr);
+printf("%s", str);
+
 return (0);
 }
