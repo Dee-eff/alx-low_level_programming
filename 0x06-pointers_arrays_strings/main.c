d92c3d6432de7ca449c3fcce49e65f80f012f650
Dee.eff <itzdaniie@gmail.com>
2023-10-19 18:19:50 -0700

    OKay

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -1,5 +1,21 @@
 #include "main.h"
 #include <stdio.h>
+void print_array(int *a, int n)
+{
+int i;
+i = 0;
+while (i < n)
+{
+if (i != 0)
+{
+printf(", ");
+}
+printf("%d", a[i]);
+i++;
+}
+printf("\n");
+}
+
 int main(void)
 {
 char s1[98] = "Hello ";
@@ -61,6 +77,11 @@ char s2[] = "World!";
 printf("%d\n", _strcmp(s1, s2));
 printf("%d\n", _strcmp(s2, s1));
 printf("%d\n", _strcmp(s1, s1));
+
+int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+print_array(a, sizeof(a) / sizeof(int));
+reverse_array(a, sizeof(a) / sizeof(int));
+print_array(a, sizeof(a) / sizeof(int));
 
 return (0);
 }
