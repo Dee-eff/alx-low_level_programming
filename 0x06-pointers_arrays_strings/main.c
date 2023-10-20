e8358e7e81066f514902ae6551e0209bb1081c8c
Dee.eff <itzdaniie@gmail.com>
2023-10-19 18:04:16 -0700

    Okay

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -55,5 +55,12 @@ printf("\n");
 printf("0x%02x", s1[i]);
 }
 printf("\n");
+
+char s1[] = "Hello";
+char s2[] = "World!";
+printf("%d\n", _strcmp(s1, s2));
+printf("%d\n", _strcmp(s2, s1));
+printf("%d\n", _strcmp(s1, s1));
+
 return (0);
 }
