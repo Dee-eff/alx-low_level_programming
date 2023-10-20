ce048b1afa5f6f9da9de91fee9ed5a7f37250a0d
Dee.eff <itzdaniie@gmail.com>
2023-10-19 18:57:50 -0700

    Okay

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -112,5 +112,16 @@ printf("%s", p);
 printf("------------------------------------\n");
 printf("%s", s);
 
+print_number(98);
+_putchar('\n');
+print_number(402);
+_putchar('\n');
+print_number(1024);
+_putchar('\n');
+print_number(0);
+_putchar('\n');
+print_number(-98);
+_putchar('\n');
+
 return (0);
 }
