58e8619a6e827da87a821d3b75a4ee2633e3e47a
Dee.eff <itzdaniie@gmail.com>
2023-11-16 08:01:57 -0800

    Okay

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,7 +10,7 @@ int check_palindrome(char *s, int start, int end);
 int is_palindrome(char *s)
 {
 int length = _strlen_recursion(s);
-return check_palindrome(s, 0, length - 1);
+return (check_palindrome(s, 0, length - 1));
 }
 
 /**
@@ -24,10 +24,10 @@ return check_palindrome(s, 0, length - 1);
 int check_palindrome(char *s, int start, int end)
 {
 if (start >= end)
-return 1;
+return (1);
 if (s[start] != s[end])
-return 0;
-return check_palindrome(s, start + 1, end - 1);
+return (0);
+return (check_palindrome(s, start + 1, end - 1));
 }
 
 /**
@@ -39,6 +39,6 @@ return check_palindrome(s, start + 1, end - 1);
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
-return 0;
-return (_strlen_recursion(s + 1) + 1);
+return (0);
+return ((_strlen_recursion(s + 1) + 1));
 }
