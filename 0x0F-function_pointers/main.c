03ca9a67169c8adb354c7795e0adf93ac9132e1a
Dee.eff <itzdaniie@gmail.com>
2023-12-01 12:48:50 -0800

    OKay

--- 0x0F-function_pointers/main.c
+++ 0x0F-function_pointers/main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "function_pointers.h"
 #include <stdio.h>
 void print_name_uppercase(char *name)
 {
@@ -26,7 +26,7 @@ i++;
  */
 int main(void)
 {
-print_name("Bob", print_name_as_is);
+print_name("Bob", print_name_uppercase);
 print_name("Bob Dylan", print_name_uppercase);
 printf("\n");
 return (0);
