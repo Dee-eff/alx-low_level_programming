f42b82427813c9e1415c38d45e2b656841e3c6da
Dee.eff <itzdaniie@gmail.com>
2023-12-01 12:37:35 -0800

    OKay

---
+++ 0x0F-function_pointers/main.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+void print_name_uppercase(char *name)
+{
+unsigned int i;
+printf("Hello, my uppercase name is ");
+i = 0;
+while (name[i])
+{
+if (name[i] >= 'a' && name[i] <= 'z')
+{
+putchar(name[i] + 'A' - 'a');
+}
+else
+{
+putchar(name[i]);
+}
+i++;
+}
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_name("Bob", print_name_as_is);
+print_name("Bob Dylan", print_name_uppercase);
+printf("\n");
+return (0);
+}
